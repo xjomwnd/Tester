7e09e8e7956d53133a70040fc520c569c4bd6974
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 13:47:50 +0300

    commit

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var app = express();
-
-mongodb.initDb((err) => {
-    if (err) {
-      console.log(err);
-    } 
-  });
-
+// Middleware for parsing JSON and urlencoded form data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', users);
+// Route for serving the HTML form
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/submit" method="post">
+      <label for="data">Enter Data:</label><br>
+      <input type="text" id="data" name="data"><br>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+// Route for handling form submission
+app.post('/submit', (req, res) => {
+  const { data } = req.body;
+  // Here you can process the submitted data, e.g., save it to a database
+  res.send(`Data received: ${data}`);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
