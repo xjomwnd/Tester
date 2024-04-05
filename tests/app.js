44aa4678707f86bd2933e1e5a0bf9316e8c03785
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 19:03:30 +0300

    commit

--- tests/app.js
+++ tests/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
 
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
@@ -14,3 +15,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app; // Export the Express app instance
