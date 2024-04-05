ba12374f18495d62cf243560ea5a8611ec26f496
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 15:43:49 +0300

    commit

--- app.test.js
+++ app.test.js
@@ -1,29 +1,21 @@
-const request = require('supertest');
-const app = require('./app');
+// App.test.js
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
+import App from './App';
 
-describe('Test the root path', () => {
-  test('It should respond to the GET method with status code 200', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
+jest.mock('axios');
 
-  test('It should respond to the GET method with a form', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toContain('<form');
-  });
+test('submits test form', async () => {
+  axios.post.mockResolvedValueOnce({ data: { success: true } });
 
-  test('It should respond to the POST method with status code 200', async () => {
-    const response = await request(app)
-      .post('/submit')
-      .send({ data: 'Test data' });
-    expect(response.statusCode).toBe(200);
-  });
+  render(<App />);
 
-  test('It should respond to the POST method with the submitted data', async () => {
-    const testData = 'Test data';
-    const response = await request(app)
-      .post('/submit')
-      .send({ data: testData });
-    expect(response.text).toContain(`Data received: ${testData}`);
-  });
+  fireEvent.change(screen.getByLabelText('Test Name:'), { target: { value: 'Test' } });
+  fireEvent.change(screen.getByLabelText('Test Score:'), { target: { value: '100' } });
+
+  fireEvent.click(screen.getByText('Submit'));
+
+  await waitFor(() => expect(screen.getByText('Test submitted successfully!')).toBeInTheDocument());
 });
