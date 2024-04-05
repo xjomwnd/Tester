29cd9099f2e33d3b1b9aeed8071093818724edab
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-05 17:24:26 +0300

    COMMIT

---
+++ submitForm.test.js
@@ -0,0 +1,8 @@
+// submitForm.test.js
+
+const request = require('supertest');
+const app = require('./app');
+
+test('submit form test with Supertest', async () => {
+  // Your test code using Supertest
+});
